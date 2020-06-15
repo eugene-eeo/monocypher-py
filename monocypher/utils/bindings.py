96aafe039526e88e04c532729525c58cce578293
Eeo Jun <141bytes@gmail.com>
2020-06-15 13:29:53 +0100

    add bindings export

---
+++ monocypher/utils/bindings.py
@@ -0,0 +1,31 @@
+from .crypto_aead import crypto_lock, crypto_unlock, CryptoError
+from .crypto_cmp import crypto_verify16, crypto_verify32, crypto_verify64
+from .crypto_hash import (
+    crypto_blake2b,
+    crypto_blake2b_init, crypto_blake2b_update, crypto_blake2b_final,
+    BLAKE2B_HASH_MIN, BLAKE2B_HASH_MAX,
+    BLAKE2B_KEY_MIN, BLAKE2B_KEY_MAX,
+)
+from .crypto_public import (
+    crypto_key_exchange, crypto_key_exchange_public_key,
+    crypto_x25519, crypto_x25519_public_key,
+)
+from .crypto_pwhash import crypto_argon2i
+from .crypto_sign import (
+    crypto_sign_public_key, crypto_sign, crypto_check,
+    crypto_from_eddsa_private, crypto_from_eddsa_public,
+)
+
+__all__ = [
+    'crypto_lock', 'crypto_unlock', 'CryptoError',
+    'crypto_verify16', 'crypto_verify32', 'crypto_verify64',
+    'crypto_blake2b',
+    'crypto_blake2b_init', 'crypto_blake2b_update', 'crypto_blake2b_final',
+    'BLAKE2B_HASH_MIN', 'BLAKE2B_HASH_MAX',
+    'BLAKE2B_KEY_MIN', 'BLAKE2B_KEY_MAX',
+    'crypto_key_exchange', 'crypto_key_exchange_public_key',
+    'crypto_x25519', 'crypto_x25519_public_key',
+    'crypto_argon2i',
+    'crypto_sign_public_key', 'crypto_sign', 'crypto_check',
+    'crypto_from_eddsa_private', 'crypto_from_eddsa_public',
+]
