4bd8e28a001d6d3c46e0949fd8b5e9c449d84adb
Eeo Jun <141bytes@gmail.com>
2020-06-18 22:50:44 +0100

    fix config

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,4 +3,4 @@ from hypothesis import settings
 
 settings.register_profile('dev', max_examples=100)
 settings.register_profile('ci', max_examples=1000)
-settings.load_profile(os.getenv(u'HYPOTHESIS_PROFILE', 'dev'))
+settings.load_profile(os.getenv('HYPOTHESIS_PROFILE', 'dev'))
