ea4dffa338692c19d8d0b37d4e8c383b5468257c
foonoxous <RWQe4qDs7LIsmI7Q94ctNMSfwfDW8eld24YGevEzQEKZyfJz1NPlH6uf>
2022-01-16 13:47:30 +0000

    Remove compile flags for better compatibility.

--- src/build.py
+++ src/build.py
@@ -19,7 +19,6 @@ ffi.set_source(
     ''',
     sources=sources,
     include_dirs=[include_dir],
-    extra_compile_args=['-std=c99', '-O3', '-march=native'],
 )
 with open(cdefs_file, 'r') as fp:
     ffi.cdef(fp.read())
